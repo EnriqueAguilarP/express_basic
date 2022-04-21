66c05394d315518aa5d31a8c5d1ed84d4af6935c
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-21 17:57:07 -0500

    regresar un objeto a través de url

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a tu primer servidor')
 })
 
+//respuesta a localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenido NodeJs en LaunchX')
+})
+
+//regresar un objeto
+//localhost:3000/exploresInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: 'explorer', msg: 'Hello'}
+    res.send(explorer)
+})
+
 //Inicializar esta app
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`)
